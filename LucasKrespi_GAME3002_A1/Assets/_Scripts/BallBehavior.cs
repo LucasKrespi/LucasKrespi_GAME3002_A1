32863ffb0479402d9594f20b93f70bea40eb6488
lucaskrespi <lucas.krespi@gmail.com>
2021-02-12 14:30:45 -0500

    ball movement added/Field added

---
+++ LucasKrespi_GAME3002_A1/Assets/_Scripts/BallBehavior.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallBehavior : MonoBehaviour
+{
+    Rigidbody Ball;
+    Vector3 mouseUpPos;
+    Vector3 mouseDownPos;
+    private float MAX_FORCE = 800.0f;
+    private float mouseDownTimeStart;
+    private float mouseDownTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Ball = GetComponent<Rigidbody>();
+    }
+
+    public void apllyForce(Vector3 direction, float force)
+    {
+        Ball.AddForce((direction * force));
+    }
+
+    private float calculateForce(float Time)
+    {
+        float maxHoldTime = 2.0f;
+        float normalizedHoldTime = Mathf.Clamp01(Time / maxHoldTime);
+        float force = normalizedHoldTime * MAX_FORCE;
+
+        return force;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+    
+        if (Input.GetMouseButtonDown(0))
+        {
+            mouseDownPos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 100) ;
+            mouseDownTimeStart = Time.time;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            mouseDownTime = Time.time - mouseDownTimeStart;
+            mouseUpPos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0);
+
+            apllyForce((mouseDownPos - mouseUpPos).normalized,calculateForce(mouseDownTime));
+        }
+    }
+
+}
---
+++ LucasKrespi_GAME3002_A1/Assets/_Scripts/GM.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GM : MonoBehaviour
+{
+    [SerializeField] private BallBehavior ball;
+    [SerializeField] private Transform ballTransform;
+    Vector3 mouse;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+       
+    }
+
+    private Vector3 CalculateDirection()
+    {
+        Vector3 temp;
+        Vector3 mousePos;
+        Vector3 PlayerPos;
+
+        mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane)).normalized;
+        PlayerPos = ballTransform.position.normalized;
+
+        temp = - mousePos + PlayerPos;
+        
+        return temp;
+    }
+
+   
+
+}
